cb237679b0a646414ddbd195e6cc0f010a8a9ead
Seba Gamboa <me@sagmor.com>
2020-11-07 05:38:07 +0000

    Add base project  scaffolding

---
+++ src/Runtimes/Monolith/Program.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace HawkLab.Courier.Runtimes.Monolith {
+    public static class Program {
+      static void Main() {
+          Console.WriteLine("Hello World");
+      }
+    }
+}

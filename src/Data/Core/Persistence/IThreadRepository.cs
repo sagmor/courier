ef184e862906ed89ed260539945a676e78772389
Seba Gamboa <me@sagmor.com>
2020-12-21 05:04:46 +0000

    Add missing methods to IThreadRepository interface

--- src/Data/Core/Persistence/IThreadRepository.cs
+++ src/Data/Core/Persistence/IThreadRepository.cs
@@ -1,17 +1,18 @@
 namespace HawkLab.Data.Core.Persistence
 {
     using System.Collections.Generic;
-    using System.Threading.Tasks;
     using HawkLab.Data.Core.Types;
-    
+
     public interface IThreadRepository
     {
+        Thread GetById(int id);
+
+        IEnumerable<Thread> GetThreadsBySubject(string subject = null);
 
         Thread Update(Thread updatedThread);
 
         Thread Add(Thread newThread);
+
         int Commit();
     }
-
-    
 }

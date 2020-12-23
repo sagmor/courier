e23021e31958f16edb35cef1b3b012f0af77ebcf
Carito Orellana <carito.orellana@gmail.com>
2020-12-22 18:42:57 -0800

    Connect web app to Mongo
    * Created ThreadRepository
    * Implemented GetbySubject and Add methods

---
+++ src/Data/MongoPersistence/MongoThreadRepository.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using System.Linq;
+using HawkLab.Data.Core.Persistence;
+using HawkLab.Data.Core.Types;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace HawkLab.Data.MongoPersistence
+{
+    public class MongoThreadRepository : IThreadRepository
+    {
+        public Thread Add(Thread newThread)
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("courier");
+            var collection = database.GetCollection<Thread>("threads");
+            collection.InsertOne(newThread);
+            return newThread;
+        
+        }
+
+        public int Commit()
+        {
+            return 0;
+        }
+
+        public Thread GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Thread> GetThreadsBySubject(string subject = null)
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("courier");
+            var collection = database.GetCollection<Thread>("threads");
+            var documents = collection.Find(new BsonDocument()).ToList();
+            return documents;
+        }
+
+        public Thread Update(Thread updatedThread)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+
+}
--- src/Servers/Web/Startup.cs
+++ src/Servers/Web/Startup.cs
@@ -1,12 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using HawkLab.Data.Core.Persistence;
-using HawkLab.Data.InMemoryPersistence;
+using HawkLab.Data.MongoPersistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,7 +20,7 @@ namespace HawkLab.Courier.Servers.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<IThreadRepository, InMemoryThreadRepository>();
+            services.AddSingleton<IThreadRepository, MongoThreadRepository>();
             services.AddRazorPages();
         }
 

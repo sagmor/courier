c74636018fefc875b31d303cc56801770936599e
Carito Orellana <carito.orellana@gmail.com>
2020-12-28 17:59:47 -0800

    My find and update so far

--- src/Data/MongoPersistence/MongoThreadRepository.cs
+++ src/Data/MongoPersistence/MongoThreadRepository.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using HawkLab.Data.Core.Persistence;
 using HawkLab.Data.Core.Types;
+using Microsoft.VisualBasic;
 using MongoDB.Bson;
 using MongoDB.Driver;
 
@@ -17,7 +19,6 @@ namespace HawkLab.Data.MongoPersistence
             var collection = database.GetCollection<Thread>("threads");
             collection.InsertOne(newThread);
             return newThread;
-        
         }
 
         public int Commit()
@@ -27,7 +28,19 @@ namespace HawkLab.Data.MongoPersistence
 
         public Thread GetById(Guid id)
         {
-            throw new System.NotImplementedException();
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("courier");
+            var collection = database.GetCollection<Thread>("threads");
+
+            var documents = collection.Find(new BsonDocument()).ToList();
+            var document = documents.SingleOrDefault(t => t.Id == id);
+            
+            // Failing for some reason
+            // var filter = Builders<Thread>.Filter.Eq("Id", id);
+            // var document = collection.Find(filter).First();
+            // var document = collection.Find(t => t.Id == id).Single();
+
+            return document;
         }
 
         public IEnumerable<Thread> GetThreadsBySubject(string subject = null)
@@ -41,7 +54,17 @@ namespace HawkLab.Data.MongoPersistence
 
         public Thread Update(Thread updatedThread)
         {
-            throw new System.NotImplementedException();
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("courier");
+            var collection = database.GetCollection<Thread>("threads");
+            // collection.UpdateOne(updatedThread);
+
+            var result = collection.ReplaceOne(
+                new BsonDocument("_id", updatedThread.Id), 
+                updatedThread,
+                new UpdateOptions { IsUpsert = true});
+            return updatedThread;
+            // throw new System.NotImplementedException();
         }
     }
 
